3b793dd92e3f857b474a7aa89a00e0e45f26ecb3
karmar4 <karmar.barrett97@gmail.com>
2021-12-05 13:38:33 +1100

    Add day1 solution

---
+++ day_1/challange_1.rb
@@ -0,0 +1,33 @@
+file = File.open("input.txt")
+data = file.read.split("\n").map { |x| x.to_i }
+
+# chunks = data.each_slice(3)
+
+
+def compare_the_pair(array)
+    counter = 0
+    
+    array.each_with_index do |num, index|  
+        next if index + 1 == array.length
+        
+        next_num = array[index + 1]
+        if num < next_num
+            counter += 1
+        end
+    end    
+    
+    return counter
+end
+
+
+
+summed_chunks = data.each_with_index.map do |x,i| 
+    x + data[i+1] + data[i+2] if i < data.length - 2 
+end
+summed_chunks = summed_chunks.compact
+
+
+
+print("Part 1: " + (compare_the_pair(data).to_s) + "\n")
+
+print("Part 2: " + (compare_the_pair(summed_chunks).to_s))
