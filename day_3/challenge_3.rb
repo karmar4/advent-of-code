cf7db2022f1c55f3aeb0748a9188b4982e1ff1d5
karmar4 <karmar.barrett97@gmail.com>
2021-12-06 18:59:27 +1100

    day 3, - part 2

--- day_3/challenge_3.rb
+++ day_3/challenge_3.rb
@@ -1,23 +1,48 @@
 file = File.open('input.txt')
-data = file.read.split("\n")
+input = file.read.split("\n")
 
 # part 1
 placements = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 gamma = Array.new
 
-def final_bit(list, placment, gamma)
+def most_common_bit(data, placment)
   one = 0
   zero = 0
-
-  list.each { |x| x[placment] == '0' ? zero += 1 : one += 1 }
-  zero > one ? gamma << 0 : gamma << 1
+  data.each { |x| x[placment] == '0' ? zero += 1 : one += 1 }
+  if zero > one
+    most_common = 0
+  elsif zero <= one
+    most_common = 1
+  end
 end
 
-placements.map { |x| final_bit(data, x, gamma) }
+placements.each do |pos|
+  puts input.length
+  if input.length == 1 
+  print input
+  end
+  common_bit = most_common_bit(input, pos)
+  input = input.filter {|x| x[pos].to_i == common_bit}
 
-epsilon = gamma.map { |x| 1 - x }
+end
 
-d_gamma = Integer(gamma.join.to_s, 2)
-d_epsilon = Integer(epsilon.join.to_s, 2)
+print input
+puts Integer(input.join.to_s, 2)
 
-puts d_epsilon * d_gamma
+
+# placements.map { |x| final_bit(data, x) }
+# data = data.select{ |x| x[placment].to_i == most_common }
+
+
+# epsilon = gamma.map { |x| 1 - x }
+
+# d_gamma = Integer(gamma.join.to_s, 2)
+# d_epsilon = Integer(epsilon.join.to_s, 2)
+
+# puts d_epsilon * d_gamma
+
+# part 2
+
+oxygen_generator = ["110010000101",3205]
+cO2_scrubber = ["000100011010", 282]
+puts (3205 *282).to_s
