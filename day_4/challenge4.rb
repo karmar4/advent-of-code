474e0d2eb9be7a155879a8326a3dabc796ff9ab4
karmar4 <karmar.barrett97@gmail.com>
2021-12-10 15:29:37 +1100

    day 4, - part 2

--- day_4/challenge4.rb
+++ day_4/challenge4.rb
@@ -5,6 +5,9 @@ input = file.read.split("\n")
 
 calls = input.shift.split(",")
 
+
+
+
 bingo_cards = input.map{|x| x.split(" ")}
     .reject(&:empty?)
     .each_slice(5)
@@ -19,20 +22,24 @@ def winningCard?(card, calls)
 end
 
 def calculateScore(card, calls)
-    
     sum = card.flatten().filter{|x| !calls.include?(x)}.map{|x| x.to_i}.sum()
-  
+
     print(sum * calls.last.to_i)
 
 end
 
 (1..calls.length).each do |idx|
     current_calls = calls.take(idx)
-    bingo_cards.each do |card|
-        if (winningCard?(card, current_calls))
-            score = calculateScore(card, current_calls)
-            puts (score)
-            return
+    if bingo_cards.to_a.length == 1 && (winningCard?(bingo_cards.first, current_calls))
+            pp bingo_cards.first
+            calculateScore(bingo_cards.first, current_calls)
+    break 
+    else
+        bingo_cards.each do |card|
+            if (winningCard?(card, current_calls))
+                bingo_cards = bingo_cards.reject{|c| c == card}
+            end
+
         end
     end
 end
